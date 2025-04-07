a328764d0e2e3f9567f677a4f70254319a38e978
lucas serafim <lucasfreitasserafim@hotmail.com>
2025-04-07 11:08:55 -0300

    exercicio03

---
+++ exercicio02.py
@@ -0,0 +1,3 @@
+nome = input("digite um nome: ")
+for z in range(0,3,1):
+    print(nome)
---
+++ exercicio03.py
@@ -0,0 +1,5 @@
+for i in range(1,11,1):
+    print(i, end=" ")
+
+    #end=" "   faz o separamento de quebra de linha
+    
