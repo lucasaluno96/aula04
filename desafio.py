71c4109ccef072a186f9dcc1182642c5cde325a6
lucas serafim <lucasfreitasserafim@hotmail.com>
2025-04-08 14:36:30 -0300

    desafio

---
+++ desafio.py
@@ -0,0 +1,3 @@
+num = int(input("digite um numero: "))
+for x in range(1, num + 1, 2):
+    print(x)
--- exercicio05.py
+++ exercicio05.py
@@ -2,3 +2,4 @@ num = int(input("digite um numero: "))
 for x in range(1,11,1):
     res = x * num
     print(f"{x} * {num} = {res}")
+resto =
